415b6cf37a83004e9ab88c608c5c78f49763515c
Paul Albee <paul@albee.com>
2022-12-21 23:21:28 -0800

    Fixed error in name

--- holiday_card/hershey_text.py
+++ holiday_card/hershey_text.py
@@ -9,21 +9,15 @@ START_Y = 0
 
 
 def construct_message(message, characters, center=False):
-    x_strokes = None
-    y_strokes = None
-    x_start = -1
-    prev_right = -1
+    x_strokes = []
+    y_strokes = []
+    x_start = 0
+    prev_right = 0
 
     for c in message:
         char = characters[ord(c) - ord(' ')]
-        if x_strokes is None:
-            x_strokes = []
-            y_strokes = []
-            x_start = -char['left']
-            prev_right = char['right']
-        else:
-            x_start += prev_right + -char['left']
-            prev_right = char['right']
+        x_start += prev_right + -char['left']
+        prev_right = char['right']
         for x, y in zip(char['x_strokes'], char['y_strokes']):
             x_strokes.append(x + x_start)
             y_strokes.append(y)
@@ -33,6 +27,27 @@ def construct_message(message, characters, center=False):
         x_strokes = [x - x_center for x in x_strokes]
 
     return x_strokes, y_strokes
+
+
+def construct_bboxes(message, characters, center=False):
+    x_strokes = []
+    y_strokes = []
+    x_start = 0
+    prev_right = 0
+
+    for c in message:
+        char = characters[ord(c) - ord(' ')]
+        x_start += prev_right + -char['left']
+        x_stop = x_start + char['right']
+        prev_right = char['right']
+        x_strokes.append(np.array([x_start + char['left'], x_stop, x_stop, x_start + char['left']]))
+        y_strokes.append(np.array([char['bottom'], char['bottom'], char['top'], char['top']]))
+    if center:
+        x_center = (x_start + prev_right) / 2.0
+        x_strokes = [x - x_center for x in x_strokes]
+
+    return x_strokes, y_strokes
+
 
 def load_font(file_name):
     """Load a font file.
@@ -134,11 +149,16 @@ This is accomplished by changing the second (y) elemement of each vertex to be c
 
 
 def transform_message(x_strokes, y_strokes, height=1, base_x=0, base_y=0, rotation=0):
+    return transform_strokes(x_strokes, y_strokes, width=height, height=height, base_x=base_x,
+                             base_y=base_y, rotation=rotation)
+
+
+def transform_strokes(x_strokes, y_strokes, width=1, height=1, base_x=0, base_y=0, rotation=0):
     transformed_x = []
     transformed_y = []
     for segments_x, segments_y in zip(x_strokes, y_strokes):
         # Scale
-        segments_x = np.array(segments_x, dtype=float) * height
+        segments_x = np.array(segments_x, dtype=float) * width
         segments_y = np.array(segments_y, dtype=float) * height
 
         # Rotate
@@ -152,3 +172,23 @@ def transform_message(x_strokes, y_strokes, height=1, base_x=0, base_y=0, rotati
         transformed_x.append(segments_x)
         transformed_y.append(segments_y)
     return transformed_x, transformed_y
+
+
+def vert_squiggle(cycles=1.0):
+    n_samples = np.ceil(30. * cycles).astype(int)
+    y = np.linspace(0, 1, num=n_samples, endpoint=True)
+    x_strokes = [(np.sin(y * np.pi * 2 * cycles) + 1) / 2.0]
+    y_strokes = [np.array(y)]
+    return x_strokes, y_strokes
+
+
+def cross_out_bbox(x_bbox, y_bbox, cycles=1):
+    x_squig, y_squig = vert_squiggle(cycles)
+
+    base_x = x_bbox[0]
+    base_y = y_bbox[0]
+    width = x_bbox[1] - x_bbox[0]
+    height = y_bbox[2] - y_bbox[1]
+    x_squig, y_squig = transform_strokes(x_squig, y_squig, base_x=base_x, base_y=base_y,
+                                         width=width, height=height)
+    return x_squig, y_squig
